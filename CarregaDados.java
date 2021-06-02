eedacab3f20e5dd8a00a908d336a18829cbfee1f
bramos013 <brunoramos013@gmail.com>
2021-06-01 21:12:03 -0300

    Correcao metodos de consulta no qual retornavam null

--- CarregaDados.java
+++ CarregaDados.java
@@ -3,7 +3,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 public class CarregaDados{
     private List<Onibus> frota = new ArrayList<>();    
 
@@ -37,21 +36,23 @@ public class CarregaDados{
     
         //Metodo de consulta da Frota Cadastrada
     public  String consultaFrota(){                
+        String res = "";
         if(frota.size()==0){
             System.out.println("\t\tNenhuma Informação a exibir!");
-            return null;
+           // return null;
         }for(int i=0 ; i < frota.size(); i++){
-            System.out.println(frota.get(i).toString());
+            //System.out.println(frota.get(i).toString());
+            res += "\n" + frota.get(i).toString();
         }
-        return null;
+        return res;
     }
     
         //Metodos de consulta utilizando Prefixo
     public  String consultaPrefixo(int prefixo){
-        String res = null;
+        String res = "";
             for (Onibus onibus : frota) {
                 if(onibus.getPrefixo() == prefixo){
-                    System.out.println(onibus.toString());
+                    res += "\n" + onibus.toString();
                 }
             }
         return res;
@@ -59,43 +60,42 @@ public class CarregaDados{
 
         //Metodo de consulta utilizando  Empresa
     public  String consultaEmpresa(String empresa){
-        String res = null;
+        String res = "";
             for (Onibus onibus : frota) {
                 if(onibus.getEmpresa().equals(empresa)){
-                    System.out.println(onibus.toString());
-                }
+                    res += "\n" + onibus.toString();                }
             }
         return res;
-    }
-    
+    }    
+
         //Metodo de consulta utilizando Marca
     public  String consultaMarca(String marca){
-        String res = null;
+        String res = "";
             for (Onibus onibus : frota) {
                 if(onibus.getMarca().equals(marca)){
-                    System.out.println(onibus.toString());
+                    res += "\n" + onibus.toString();
                 }
             }
         return res;
-    }
-        
+    }        
+
         //Metodo de consulta utilizando Placa
     public  String consultaPlaca(String placa){
-        String res = null;
+        String res = "";
             for (Onibus onibus : frota) {
                 if(onibus.getPlaca().equals(placa)){
-                    System.out.println(onibus.toString());
+                    res += "\n" + onibus.toString();
                 }
             }
         return res;
-    }
-        
+    }        
+
         //Metodo de consulta utilizando Ano de Fabricação
     public  String consultaAno(int ano_fabricacao){        
-        String res = null;
+        String res = "";
             for (Onibus onibus : frota) {
                 if(onibus.getAno_fabricacao() == ano_fabricacao){
-                    System.out.println(onibus.toString());
+                    res += "\n" + onibus.toString();
                 }
             }
         return res;
